92cbb4832828366cdb3928eff0c37829f73977a4
jmgomez04 <43625766+jmgomez04@users.noreply.github.com>
2018-10-03 00:22:37 -0600

    Add files via upload

---
+++ Ejercicio 10.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html>
+<head>
+<Title> PHP </Title>
+</head>
+
+<body>
+
+<h1>Este es el ejercicio 10 utilizando PHP </h1>
+
+
+
+<form method ="post" action="backend.php">
+<label>Un campo de texto plano</label><br>
+<input type="text" name="nombre"/><br>
+
+Campo de texto<br>
+<textarea name="campo de texto" ></textarea>
+
+<input type="submit" value="enviar la  vara"
+
+  <input type="checkbox" name="check1">campo check box
+  <input type="radio" name="gender" value="Aceptar">RadioBoton1
+  <input type="radio" name="gender" value="Declinar">RadioBoton2
+  <input type="radio" name="gender" value="Enviar">RadioBoton3
+  <p>Campo tipo rango: <input type="range" name="El rango"> </p>
+
+
+</form>
+	
+
+</body>
+</html>
